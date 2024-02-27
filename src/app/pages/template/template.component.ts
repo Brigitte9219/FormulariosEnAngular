229781c1b1734893e824abf12e37f2523ff0e522
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-27 08:00:35 -0500

    Formulario template: obteniendo la información del formulario

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +8,14 @@ import { Component } from '@angular/core';
 })
 export class TemplateComponent {
 
-  guardar(){
-    console.log('submit disparado');
+  usuario = {
+    nombre:'Carlos'
   }
 
-  
+  guardar(forma:NgForm){
+    console.log(forma);
+    console.log(forma.value);
+  }
+
+
 }
