30b68db50e7eef2d8817a3e1c8d034ebf3be76ff
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-27 07:02:28 -0500

    Creación de componentes, formularios y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule//para no hacer refresh o recargar la página con el formulario
   ],
   providers: [],
   bootstrap: [AppComponent]
