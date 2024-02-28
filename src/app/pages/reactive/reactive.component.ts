9b55f6d8b4565b8f62bdd0acd756bf34aa2aa142
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-02-28 09:26:26 -0500

    Formulario Reactivo: Validaciones síncronas

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -16,9 +16,9 @@ export class ReactiveComponent {
 
   crearFormulario(){
     this.forma = this.fb.group({
-      nombre:['Brigitte'],
-      apellido:['Padilla'],
-      correo:['Brigitte@gmail.com']
+      nombre:['', Validators.required],
+      apellido:['', [Validators.required, Validators.minLength(5)]],
+      correo:['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
     });
   }
 
